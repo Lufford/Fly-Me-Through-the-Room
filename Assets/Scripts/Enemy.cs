5683f04fd38ac7e5df10203951d116ab18ce4ca4
Lufford <146119753+Lufford@users.noreply.github.com>
2024-11-10 18:27:36 -0500

    Changed enemy tracking, added random enemy spawn amount and speed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,18 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float minSpeed, maxSpeed, chaseSpeed = 1f;
+    private Rigidbody2D rb;
+    private Transform target;
+    Vector2 moveDirection;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        float speed = Random.Range(minSpeed, maxSpeed);
+        rb.velocity = new Vector2(rb.velocity.x, -speed);
+    }
+
+    private void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x * chaseSpeed, rb.velocity.y);
+        }
         
     }
     
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Enemy;
+    private float spawnRate = 0f;
+    private float spawnTime = 0f;
+    private int spawnAmount = 0;
+    
+    
     private static GameManager instance;
     public static GameManager Instance
     {
@@ -21,5 +29,30 @@ public class GameManager : MonoBehaviour
 
             return instance;
         }
+    }
+    
+    private void spawnEnemy()
+    {
+        spawnRate = Random.Range(5, 10);
+        spawnAmount = Random.Range(1, 4);
+        if (Time.time > spawnTime)
+        {
+            for (int i = spawnAmount; i <= 4; i++)
+            {
+                Instantiate(Enemy, new Vector3(Random.Range(-2,2), Random.Range(6,15), 0f), Quaternion.identity);
+            }
+            
+            spawnTime = Time.time + spawnRate;
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        spawnEnemy();
     }
 }
